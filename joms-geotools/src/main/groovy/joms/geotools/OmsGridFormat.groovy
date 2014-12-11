26acf66702ed93171542a2a3733dbdcf965bee0c
Garrett Potts <gpotts@radiantblue.com>
2014-12-11 14:26:15 -0500

    Added mInfo settings and reader parameter settings

--- joms-geotools/src/main/groovy/joms/geotools/OmsGridFormat.groovy
+++ joms-geotools/src/main/groovy/joms/geotools/OmsGridFormat.groovy
@@ -4,15 +4,22 @@ import org.geotools.coverage.grid.io.AbstractGridCoverage2DReader
 import org.geotools.coverage.grid.io.AbstractGridFormat
 import org.geotools.coverage.grid.io.imageio.GeoToolsWriteParams
 import org.geotools.factory.Hints
+import org.geotools.parameter.ParameterGroup
 import org.opengis.coverage.grid.GridCoverageWriter
+import org.geotools.parameter.DefaultParameterDescriptorGroup;
 
 import joms.oms.DataInfo
+import org.opengis.parameter.GeneralParameterDescriptor
 
 /**
  * Created by sbortman on 10/22/14.
  */
 class OmsGridFormat extends AbstractGridFormat
 {
+  OmsGridFormat() {
+    setInfo();
+  }
+
   @Override
   AbstractGridCoverage2DReader getReader(Object source, Hints hints = null)
   {
@@ -24,7 +31,25 @@ class OmsGridFormat extends AbstractGridFormat
   {
     return DataInfo.readInfo( source?.toString() ) != null
   }
+  protected void setInfo() {
+    final HashMap<String, String> info = new HashMap<String, String>();
+    info.put("name", "OSSIM");
+    info.put("description", "OSSIM Coverage Format");
+    info.put("vendor", "OSSIM");
+    info.put("docURL", ""); // TODO: set something
+    info.put("version", "1.0");
+    mInfo = Collections.unmodifiableMap(info);
 
+    // writing parameters
+     writeParameters = null;
+
+    // reading parameters
+     readParameters = new ParameterGroup(
+             new DefaultParameterDescriptorGroup(mInfo,
+                     [AbstractGridFormat.READ_GRIDGEOMETRY2D,
+                       AbstractGridFormat.USE_JAI_IMAGEREAD,
+                       AbstractGridFormat.SUGGESTED_TILE_SIZE ] as GeneralParameterDescriptor[]));
+  }
   @Override
   GeoToolsWriteParams getDefaultImageIOWriteParameters()
   {
