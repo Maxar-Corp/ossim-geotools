151717517803880ea3c4645c752b92a943473fc7
Garrett Potts <gpotts@radiantblue.com>
2014-12-11 15:07:38 -0500

    Using base setting of input source and hints

--- joms-geotools/src/main/groovy/joms/geotools/OmsGridReader.groovy
+++ joms-geotools/src/main/groovy/joms/geotools/OmsGridReader.groovy
@@ -4,26 +4,28 @@ import geoscript.geom.Bounds
 import joms.geotools.OmsGridFormat
 import org.geotools.coverage.grid.GridCoverage2D
 import org.geotools.coverage.grid.io.AbstractGridCoverage2DReader
+import org.geotools.coverage.grid.io.GridCoverage2DReader
 import org.geotools.coverageio.BaseGridCoverage2DReader
 import org.geotools.data.DataSourceException
 import org.geotools.factory.Hints
 import org.opengis.coverage.grid.Format
 import org.opengis.parameter.GeneralParameterValue
-
+import joms.geotools.ChipperUtil
 import javax.imageio.spi.ImageReaderSpi
 import java.awt.image.BufferedImage
 
 /**
  * Created by sbortman on 10/22/14.
  */
-class OmsGridReader extends AbstractGridCoverage2DReader
+class OmsGridReader extends AbstractGridCoverage2DReader implements GridCoverage2DReader
 {
   OmsGridFormat format
 
-  def source
-  def hints
+  //def source
+  //def hints
 
-  public OmsGridReader(def source, def hints)
+
+  OmsGridReader(def source, def hints)
   {
     this.source = source
     this.hints = hints
@@ -43,12 +45,14 @@ class OmsGridReader extends AbstractGridCoverage2DReader
   @Override
   GridCoverage2D read(GeneralParameterValue[] parameters) throws IllegalArgumentException, IOException
   {
+    println "akdjfhkajsdhfkajhdfkajhsdfkjhasdkfjhaksdfjhaksdjfhjashdfvbajfhbv"
     def width
     def height
     def minX
     def minY
     def maxX
     def maxY
+    def epsg = "epsg:4326"
 
     parameters?.eachWithIndex { param, i -> 
        switch ( param.descriptor.name )
@@ -72,7 +76,24 @@ class OmsGridReader extends AbstractGridCoverage2DReader
        }            
     }
 
-    def image = new BufferedImage( width, height, BufferedImage.TYPE_INT_ARGB )
+    def chipperOptionsMap = [
+            cut_wms_bbox:"${minx},${miny},${maxx},${maxy}" as String,
+            cut_height: "${height}" as String,
+            cut_width: "${width}" as String,
+            //'hist-op': 'auto-minmax',
+            //'hist-op': meta.histogramOperationType,
+            operation: 'ortho',
+            scale_2_8_bit: 'true',
+            'srs': epsg,
+            three_band_out: 'true',
+            resampler_filter: 'bilinear',
+            "image0.file":this.input.toString()
+    ]
+    def hints = [width:width,height:height,transparent:true]
+
+    println "Options: \n${chipperOptionsMap}\nHints:\n${hints}"
+    def image = ChipperUtil.createImage(chipperOptionsMap, hints)
+//    def image = new BufferedImage( width, height, BufferedImage.TYPE_INT_ARGB )
     def bbox = new Bounds( minX, minY, maxX, maxY ).env
 
     return coverageFactory.create( image, bbox )
