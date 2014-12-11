c2ca8f0c2cd88ad6c7e9a3bba818641244d0bb05
Garrett Potts <gpotts@radiantblue.com>
2014-12-11 10:23:32 -0500

    Got rid of errors

--- joms-geotools/src/main/groovy/joms/geotools/OmsGridReader.groovy
+++ joms-geotools/src/main/groovy/joms/geotools/OmsGridReader.groovy
@@ -1,15 +1,19 @@
 package joms.geotools
 
 import org.geotools.coverage.grid.GridCoverage2D
-import org.geotools.coverage.grid.io.GridCoverage2DReader
+import org.geotools.coverage.grid.io.AbstractGridCoverage2DReader
 import org.geotools.coverageio.BaseGridCoverage2DReader
+import org.geotools.data.DataSourceException
+import org.geotools.factory.Hints
 import org.opengis.coverage.grid.Format
 import org.opengis.parameter.GeneralParameterValue
+
+import javax.imageio.spi.ImageReaderSpi
 
 /**
  * Created by sbortman on 10/22/14.
  */
-class OmsGridReader extends  BaseGridCoverage2DReader implements GridCoverage2DReader {
+class OmsGridReader extends AbstractGridCoverage2DReader
 {
   OmsGridFormat format
 
