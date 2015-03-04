4c0aa303f721fa07f7279414da9cb8e09cad8536
Scott Bortman <scott.bortman@gmail.com>
2015-03-04 15:51:27 -0500

    Added dependencies on twitter-bootstrap & font-awesome

--- tilecache/grails-app/conf/BuildConfig.groovy
+++ tilecache/grails-app/conf/BuildConfig.groovy
@@ -137,6 +137,9 @@ grails.project.dependency.resolution = {
     runtime ":database-migration:1.4.0"
     runtime ":jquery:1.11.1"
 
+    runtime ":twitter-bootstrap:3.3.2.1"
+    runtime ":font-awesome-resources:4.2.0.0"
+
     // Uncomment these to enable additional asset-pipeline capabilities
     //compile ":sass-asset-pipeline:1.9.0"
     //compile ":less-asset-pipeline:1.10.0"
