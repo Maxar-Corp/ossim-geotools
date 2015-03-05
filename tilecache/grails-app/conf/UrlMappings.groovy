c9854e9ff963ad5a7130c82ff4b3022ad4f19bb5
Scott Bortman <scott.bortman@gmail.com>
2015-03-05 10:08:42 -0500

    Added redirect to app controller

--- tilecache/grails-app/conf/UrlMappings.groovy
+++ tilecache/grails-app/conf/UrlMappings.groovy
@@ -1,22 +1,23 @@
-class UrlMappings {
+class UrlMappings
+{
 
   static mappings = {
     // remove the format overide for the getTile action
-    "/accumuloProxy/getTile/$id?"(action:"getTile", controller:"accumuloProxy"){
+    "/accumuloProxy/getTile/$id?"( action: "getTile", controller: "accumuloProxy" ) {
     }
-    "/accumuloProxy/getTiles/$id?"(action:"getTiles", controller:"accumuloProxy"){
+    "/accumuloProxy/getTiles/$id?"( action: "getTiles", controller: "accumuloProxy" ) {
     }
-    "/accumuloProxy/wms/$id?"(action:"wms", controller:"accumuloProxy"){
+    "/accumuloProxy/wms/$id?"( action: "wms", controller: "accumuloProxy" ) {
     }
-    "/accumuloProxy/wmts/$id?"(action:"wmts", controller:"accumuloProxy"){
+    "/accumuloProxy/wmts/$id?"( action: "wmts", controller: "accumuloProxy" ) {
     }
-    "/$controller/$action?/$id?(.$format)?"{
+    "/$controller/$action?/$id?(.$format)?" {
       constraints {
         // apply constraints here
       }
     }
 
-    "/"(view:"/index")
-    "500"(view:'/error')
+    "/"( controller: "app" )
+    "500"( view: '/error' )
   }
 }
