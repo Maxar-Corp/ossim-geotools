40ea08ded382ef994ab7bfd8b7864d938dcccc7e
melbsurfer <melbsurfer@gmail.com>
2015-03-24 12:46:33 -0400

    Modified coordinate output <div> to match the input LonLat --> LatLon

--- tilecache/grails-app/assets/javascripts/app/client/layerManagerClient.js
+++ tilecache/grails-app/assets/javascripts/app/client/layerManagerClient.js
@@ -1,5 +1,7 @@
-LayerManagerClient = (function ()
-{
+LayerManagerClient = (function () {
+
+    // Name the root layer group
+    //AppClient.map.getLayerGroup().set('name', 'Root');
 
     /**
      * Build a tree layer from the map layers with visible and opacity
@@ -8,10 +10,9 @@ LayerManagerClient = (function ()
      * @param {type} layer
      * @returns {String}
      */
-    function buildLayerTree( layer )
-    {
+    function buildLayerTree(layer) {
         var elem;
-        var name = layer.get( 'name' ) || "Group";
+        var name = layer.get('name') || "Group";
 
         //console.log( name );
 
@@ -19,19 +20,16 @@ LayerManagerClient = (function ()
             "<span><i class='glyphicon glyphicon-file'></i> " + name + "</span>" +
             "<i class='glyphicon glyphicon-check'></i> " +
             "<input style='width:100px;' class='opacity' type='text' value='' data-slider-min='0' data-slider-max='1' data-slider-step='0.1' data-slider-tooltip='hide'>";
-        if ( layer.getLayers )
-        {
+        if (layer.getLayers) {
             var sublayersElem = '';
             var layers = layer.getLayers().getArray(),
                 len = layers.length;
-            for ( var i = len - 1; i >= 0; i-- )
-            {
-                sublayersElem += buildLayerTree( layers[i] );
+            for (var i = len - 1; i >= 0; i--) {
+                sublayersElem += buildLayerTree(layers[i]);
             }
             elem = div + " <ul>" + sublayersElem + "</ul></li>";
         }
-        else
-        {
+        else {
             elem = div + " </li>";
         }
         return elem;
@@ -41,28 +39,24 @@ LayerManagerClient = (function ()
      * Initialize the tree from the map layers
      * @returns {undefined}
      */
-    function initializeTree()
-    {
+    function initializeTree() {
 
-        var elem = buildLayerTree( AppClient.map.getLayerGroup() );
-        $( '#layertree' ).empty().append( elem );
+        var elem = buildLayerTree(AppClient.map.getLayerGroup());
+        $('#layertree').empty().append(elem);
 
-        $( '.tree li:has(ul)' ).addClass( 'parent_li' ).find( ' > span' ).attr( 'title', 'Collapse this branch' );
-        $( '.tree li.parent_li > span' ).on( 'click', function ( e )
-        {
-            var children = $( this ).parent( 'li.parent_li' ).find( ' > ul > li' );
-            if ( children.is( ":visible" ) )
-            {
-                children.hide( 'fast' );
-                $( this ).attr( 'title', 'Expand this branch' ).find( ' > i' ).addClass( 'glyphicon-plus' ).removeClass( 'glyphicon-minus' );
+        $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+        $('.tree li.parent_li > span').on('click', function (e) {
+            var children = $(this).parent('li.parent_li').find(' > ul > li');
+            if (children.is(":visible")) {
+                children.hide('fast');
+                $(this).attr('title', 'Expand this branch').find(' > i').addClass('glyphicon-plus').removeClass('glyphicon-minus');
             }
-            else
-            {
-                children.show( 'fast' );
-                $( this ).attr( 'title', 'Collapse this branch' ).find( ' > i' ).addClass( 'glyphicon-minus' ).removeClass( 'glyphicon-plus' );
+            else {
+                children.show('fast');
+                $(this).attr('title', 'Collapse this branch').find(' > i').addClass('glyphicon-minus').removeClass('glyphicon-plus');
             }
             e.stopPropagation();
-        } );
+        });
     }
 
     /**
@@ -72,25 +66,20 @@ LayerManagerClient = (function ()
      * @param {any} value
      * @returns {ol.layer.Base}
      */
-    function findBy( layer, key, value )
-    {
+    function findBy(layer, key, value) {
 
-        if ( layer.get( key ) === value )
-        {
+        if (layer.get(key) === value) {
             return layer;
         }
 
         // Find recursively if it is a group
-        if ( layer.getLayers )
-        {
+        if (layer.getLayers) {
 //            console.log(layer.getLayers);
             var layers = layer.getLayers().getArray(),
                 len = layers.length, result;
-            for ( var i = 0; i < len; i++ )
-            {
-                result = findBy( layers[i], key, value );
-                if ( result )
-                {
+            for (var i = 0; i < len; i++) {
+                result = findBy(layers[i], key, value);
+                if (result) {
                     return result;
                 }
             }
@@ -100,46 +89,40 @@ LayerManagerClient = (function ()
     }
 
     return {
-        initialize: function ( layerManagerClientParams )
-        {
-            $( '[data-toggle="tooltip"]' ).tooltip();
+        initialize: function (layerManagerClientParams) {
+            $('[data-toggle="tooltip"]').tooltip();
 
             initializeTree();
 
             // Handle opacity slider control
-            $( 'input.opacity' ).slider().on( 'slide', function ( ev )
-            {
-                var layername = $( this ).closest( 'li' ).data( 'layerid' );
-                var layer = findBy( AppClient.map.getLayerGroup(), 'name', layername );
+            $('input.opacity').slider().on('slide', function (ev) {
+                var layername = $(this).closest('li').data('layerid');
+                var layer = findBy(AppClient.map.getLayerGroup(), 'name', layername);
                 //console.log(ev.value);
 
-                layer.setOpacity( ev.value );
-            } );
+                layer.setOpacity(ev.value);
+            });
 
             // Handle visibility control
-            $( 'i' ).on( 'click', function ()
-            {
-                var layername = $( this ).closest( 'li' ).data( 'layerid' );
+            $('i').on('click', function () {
+                var layername = $(this).closest('li').data('layerid');
 
-                if ( layername )
-                {
-                    var layer = findBy( AppClient.map.getLayerGroup(), 'name', layername );
+                if (layername) {
+                    var layer = findBy(AppClient.map.getLayerGroup(), 'name', layername);
 //                console.log(AppClient.map.getLayerGroup());
-                    layer.setVisible( !layer.getVisible() );
+                    layer.setVisible(!layer.getVisible());
 
-                    if ( layer.getVisible() )
-                    {
-                        $( this ).removeClass( 'glyphicon-unchecked' ).addClass( 'glyphicon-check' );
+                    if (layer.getVisible()) {
+                        $(this).removeClass('glyphicon-unchecked').addClass('glyphicon-check');
                     }
-                    else
-                    {
-                        $( this ).removeClass( 'glyphicon-check' ).addClass( 'glyphicon-unchecked' );
+                    else {
+                        $(this).removeClass('glyphicon-check').addClass('glyphicon-unchecked');
                     }
                 }
-            } );
+            });
 
             // Name the root layer group
-            AppClient.map.getLayerGroup().set( 'name', 'Root' );
+            AppClient.map.getLayerGroup().set('name', 'Root');
         }
     };
 })();
