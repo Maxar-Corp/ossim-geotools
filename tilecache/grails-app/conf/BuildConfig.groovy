94e6c978f6c266165825a8dc888839a9ea7b74d7
Garrett Potts <gpotts@radiantblue.com>
2015-03-31 14:49:09 -0400

    Added rabbitmq plugin

--- tilecache/grails-app/conf/BuildConfig.groovy
+++ tilecache/grails-app/conf/BuildConfig.groovy
@@ -124,7 +124,7 @@ grails.project.dependency.resolution = {
 
   plugins {
     build ":tomcat:7.0.55.3"
-
+     compile ":rabbitmq-native:3.1.0"
     // plugins for the compile step
     compile ":scaffolding:2.1.2"
     compile ':cache:1.1.8'
