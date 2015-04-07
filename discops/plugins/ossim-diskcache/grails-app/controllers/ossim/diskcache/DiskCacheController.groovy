d4649c73f89bfa609eb2e100a8d1bb4456473fc8
Garrett Potts <gpotts@radiantblue.com>
2015-04-07 11:12:33 -0400

    Added Controller and service

---
+++ discops/plugins/ossim-diskcache/grails-app/controllers/ossim/diskcache/DiskCacheController.groovy
@@ -0,0 +1,74 @@
+package ossim.diskcache
+
+import grails.converters.JSON
+import grails.converters.XML
+//import grails.plugins.springsecurity.Secured
+import org.apache.commons.collections.map.CaseInsensitiveMap
+
+class DiskCacheController {
+
+  def diskCacheService
+
+  //@Secured(['ROLE_USER', 'ROLE_ADMIN'])
+  def index() {
+    render view: 'index', model:[
+            tableModel  : diskCacheService.createTableModel()
+    ]
+  }
+
+  //@Secured(['IS_AUTHENTICATED_ANONYMOUSLY'])
+  //def list(FetchDataCommand cmd){
+  def list(){
+    /*def data = diskCacheService.list( cmd )
+    response.withFormat {
+      json {
+        render contentType: 'application/json', text: data as JSON
+      }
+      xml {
+        render contentType: 'application/xml', text: data as XML
+      }
+    }
+    */
+  }
+
+  //@Secured(['ROLE_ADMIN'])
+  def create(){
+    println params
+    def data = diskCacheService.create(params)//new CaseInsensitiveMap(params));
+    response.withFormat {
+      json {
+        render contentType: 'application/json', text: data as JSON
+      }
+      xml {
+        render contentType: 'application/xml', text: data as XML
+      }
+    }
+  }
+
+  //@Secured(['ROLE_ADMIN'])
+  def update(){
+    def data = diskCacheService.update(params)//new CaseInsensitiveMap(params));
+    println  "BEST LOCATION: ${diskCacheService.getBestLocation()}"
+    response.withFormat {
+      json {
+        render contentType: 'application/json', text: data as JSON
+      }
+      xml {
+        render contentType: 'application/xml', text: data as XML
+      }
+    }
+  }
+
+  //@Secured(['ROLE_ADMIN'])
+  def remove(){
+    def data = diskCacheService.remove(new CaseInsensitiveMap(params));
+    response.withFormat {
+      json {
+        render contentType: 'application/json', text: data as JSON
+      }
+      xml {
+        render contentType: 'application/xml', text: data as XML
+      }
+    }
+  }
+}
