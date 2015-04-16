b3b65a595f066e12f44062128ce8c3d1c2d692ce
Scott Bortman <scott.bortman@gmail.com>
2015-04-16 09:02:17 -0400

    For some reason,  git seems to be wiping the contents of this file.   Trying to get it in there...

---
+++ discops/apps/tilestore/src/groovy/tilecache/wms/GetMapCommand.groovy
@@ -0,0 +1,39 @@
+package tilecache.wms
+
+import grails.validation.Validateable
+import groovy.transform.ToString
+import org.ossim.common.CaseInsensitiveBind
+
+/**
+ * Created by gpotts on 1/16/15.
+ */
+@Validateable
+@ToString( includeNames = true, includeSuper = true )
+class GetMapCommand extends WmsCommand
+{
+  String layers
+  String bbox
+  String srs
+  String format
+  Integer width
+  Integer height
+  String bgcolor = "#000000"
+  String transparent = true
+
+  static constraints = {
+    transparent( nullable: true )
+    bgcolor( nullable: true )
+    width( nullable: false, validator: { val, obj, errors ->
+      if ( ( val == null ) || ( val < 1 ) )
+      {
+        errors.reject( "width", "bad value for width" )
+      }
+    } )
+    height( nullable: false, validator: { val, obj, errors ->
+      if ( ( val == null ) || ( val < 1 ) )
+      {
+        errors.reject( "height", "bad value for height" )
+      }
+    } )
+  }
+}
