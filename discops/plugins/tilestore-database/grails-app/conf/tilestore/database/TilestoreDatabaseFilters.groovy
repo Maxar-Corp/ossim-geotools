606a1e653ac8c5b929ca4c19dde799752df98a98
Garrett Potts <gpotts@radiantblue.com>
2015-04-17 21:06:36 -0400

    Initial import or tilestore-database.  This will hold the CRUD interfaces for manipulating the tile store

---
+++ discops/plugins/tilestore-database/grails-app/conf/tilestore/database/TilestoreDatabaseFilters.groovy
@@ -0,0 +1,18 @@
+package tilestore.database
+
+class TilestoreDatabaseFilters {
+
+    def filters = {
+        all(controller:'*', action:'*') {
+            before = {
+
+            }
+            after = { Map model ->
+
+            }
+            afterView = { Exception e ->
+
+            }
+        }
+    }
+}
---
+++ discops/plugins/tilestore-database/test/unit/tilestore/database/TilestoreDatabaseFiltersSpec.groovy
@@ -0,0 +1,17 @@
+package tilestore.database
+
+import grails.test.mixin.Mock
+import spock.lang.Specification
+
+@Mock(TilestoreDatabaseFilters)
+class TilestoreDatabaseFiltersSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
