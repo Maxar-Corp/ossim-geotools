82636e668c9ef4ab9c171d4167d60ac0576252c9
Scott Bortman <scott.bortman@gmail.com>
2015-05-07 12:22:12 -0400

    FINALLY got WFS GetFeature to resolve correctly using a 3rd party client. It now works similar to OMAR's WFS which has a few caveats (not GML3+)

---
+++ discops/plugins/tilestore-wfs/src/groovy/tilestore/wfs/formats/ResultFormat.groovy
@@ -0,0 +1,15 @@
+package tilestore.wfs.formats
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sbortman
+ * Date: 2/25/13
+ * Time: 9:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+interface ResultFormat
+{
+  def getContentType()
+  def getName()
+  def getFeature(def wfsRequest, def workspace)
+}
