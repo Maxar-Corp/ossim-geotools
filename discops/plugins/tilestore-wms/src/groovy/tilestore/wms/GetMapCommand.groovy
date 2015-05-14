bf28495394ad51b716d016e8fd52d2740e2062e3
Garrett Potts <gpotts@radiantblue.com>
2015-05-14 10:19:12 -0400

    Added validator for BGCOLOR

--- discops/plugins/tilestore-wms/src/groovy/tilestore/wms/GetMapCommand.groovy
+++ discops/plugins/tilestore-wms/src/groovy/tilestore/wms/GetMapCommand.groovy
@@ -16,12 +16,43 @@ class GetMapCommand extends WmsCommand
   String format
   Integer width
   Integer height
-  String bgcolor = "#000000"
+  String bgcolor = "0x000000"
   String transparent = true
 
   static constraints = {
     transparent( nullable: true )
-    bgcolor( nullable: true )
+    bgcolor( nullable: true,
+            validator: { val, obj ->
+              def message = true
+              if ( val )
+              {
+                if ( obj.request?.toLowerCase() == "getmap" )
+                {
+                  if ( !val.startsWith( "0x" ) )
+                  {
+                    message = "BGCOLOR parameter invalid.  Value must start with 0x"
+                  }
+                  else if ( val.size() != 8 )
+                  {
+                    message = "BGCOLOR parameter invalid.  Value must be 8 characters long: example 0xFFFFFF is a white background"
+                  }
+                  else
+                  {
+                    try
+                    {
+                      Integer.decode( "0x" + val[2] + val[3] )
+                      Integer.decode( "0x" + val[4] + val[5] )
+                      Integer.decode( "0x" + val[6] + val[7] )
+                    }
+                    catch ( Exception e )
+                    {
+                      message = "BGCOLOR parameter invalid.  Individual values are not valid hex range of 00-FF"
+                    }
+                  }
+                }
+              }
+              message
+            } )
     width( nullable: false, validator: { val, obj, errors ->
       if ( ( val == null ) || ( val < 1 ) )
       {
