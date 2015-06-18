1fe4c4f24ea7e6889bcdd8239014532651a05bfd
Garrett Potts <gpotts@radiantblue.com>
2015-06-18 09:17:37 -0400

    Added modified URL mappings

--- discops/plugins/ossim-diskcache/grails-app/conf/UrlMappings.groovy
+++ discops/plugins/ossim-diskcache/grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,16 @@
 class UrlMappings {
 
 	static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
+//      group "/product",{
+//         "/export" { controller = "product"; action = "export" }
+//      }
+      "/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
             }
-        }
+       }
 
-        "/"(view:"/index")
-        "500"(view:'/error')
+      "/"(view:"/index")
+      "500"(view:'/error')
 	}
 }
