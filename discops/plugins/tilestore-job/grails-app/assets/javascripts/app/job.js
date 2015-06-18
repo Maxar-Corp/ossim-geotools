784764d2e02f38261df0351629d356763d950124
Garrett Potts <gpotts@radiantblue.com>
2015-06-18 15:08:24 -0400

    Added download js file

--- discops/plugins/tilestore-job/grails-app/assets/javascripts/app/job.js
+++ discops/plugins/tilestore-job/grails-app/assets/javascripts/app/job.js
@@ -2,6 +2,7 @@
 //= require jquery.js
 //= require common/underscore-min.js
 //= require common/backbone-min.js
+//= require common/jquery.fileDownload.js
 //= require common/jquery.easyui.min.js
 //= require common/easyui/datagrid-scrollview.js
 //= require job/jobPage.js
