c6ede445b40f139c710963756a9f9dddd03a9db0
Garrett Potts <gpotts@radiantblue.com>
2015-06-22 12:27:58 -0400

    Forgot constructor

--- ossim-tools/kettle-plugins/src/main/groovy/org/ossim/kettle/steps/plugintemplate/PluginTemplate.groovy
+++ ossim-tools/kettle-plugins/src/main/groovy/org/ossim/kettle/steps/plugintemplate/PluginTemplate.groovy
@@ -1,9 +1,12 @@
 package org.ossim.kettle.steps.plugintemplate
 
 import org.pentaho.di.core.exception.KettleException
+import org.pentaho.di.trans.Trans
+import org.pentaho.di.trans.TransMeta
 import org.pentaho.di.trans.step.BaseStep
 import org.pentaho.di.trans.step.StepDataInterface
 import org.pentaho.di.trans.step.StepInterface
+import org.pentaho.di.trans.step.StepMeta
 import org.pentaho.di.trans.step.StepMetaInterface
 
 /**
@@ -14,6 +17,10 @@ class PluginTemplate extends BaseStep implements StepInterface
    private PluginTemplateMeta meta = null;
    private PluginTemplateData data = null;
 
+   public PluginTemplate(StepMeta stepMeta, StepDataInterface stepDataInterface,
+                      int copyNr, TransMeta transMeta, Trans trans) {
+      super(stepMeta, stepDataInterface, copyNr, transMeta, trans);
+   }
    public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException
    {
       Object[] r = getRow();
