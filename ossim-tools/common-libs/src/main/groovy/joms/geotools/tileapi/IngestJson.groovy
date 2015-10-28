e55df17a8c1712bcd16a9d58f5f7f156030f7e93
Garrett Potts <gpotts@radiantblue.com>
2015-10-28 13:03:30 -0400

    Initial import

--- ossim-tools/common-libs/src/main/groovy/joms/geotools/tileapi/IngestJson.groovy
+++ ossim-tools/common-libs/src/main/groovy/joms/geotools/tileapi/IngestJson.groovy
@@ -1,8 +1,41 @@
 package joms.geotools.tileapi
 
+import groovy.json.JsonSlurper
+import groovy.transform.ToString
+
 /**
  * Created by gpotts on 10/15/15.
  */
+@ToString()
 class IngestJson
 {
+   String jobId
+   String type
+   String jobName
+   HashMap layer
+   String aoi
+   String aoiEpsg
+   Integer minLevel
+   Integer maxLevel
+   HashMap input
+
+
+   void parseString(String jsonString)
+   {
+      JsonSlurper slurper = new JsonSlurper()
+
+      def obj = slurper.parseText(jsonString)
+
+
+      jobId    = obj?.jobId
+      type     = obj?.type
+      jobName  = obj?.jobName
+      layer    = obj?.layer as HashMap
+      aoi      = obj?.aoi
+      aoiEpsg  = obj?.aoiEpsg
+      minLevel = obj?.minLevel
+      maxLevel = obj?.maxLevel
+      input    = obj?.input  as HashMap
+   }
+
 }
